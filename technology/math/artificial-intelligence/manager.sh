d48cb3ee90843c4ce83856a42c3af66e75deb8fe
朽木不可雕也 <aszswaz@163.com>
2022-03-19 21:47:06 +0800

    将 Rosenblatt 感知器的 Python 代码从 tex 中抽取出来

---
+++ technology/math/artificial-intelligence/manager.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# 文件夹的管理脚本，功能如下：
+# 1. 生成 Python 的 venv 环境，安装必要的 Python 库
+# 2. 将 tex 文件，编译为 PDF 文件，并且删除编译时产生的其他文件
+
+cd $(dirname $0)
+
+# 创建 Python 环境
+[ ! -d "./venv" ] && python -m venv venv
+source ./venv/bin/activate
+# 读取 requirements.txt 文件，安装 Python 依赖
+[ -e "./requirements.txt" ] && pip install -r "requirements.txt"
+# 生成或更新 requirements.txt 文件
+pip freeze > "requirements.txt"
+# 编译 tex 文件，生成 pdf 文件，并删除编译过程中生成的其他文件
+for item in ./*.tex; do
+    xelatex $item
+done
+rm -f *.log *.aux
