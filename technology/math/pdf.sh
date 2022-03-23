36ec62ed40fb1747f704f163dfa51c1a0fc8b6c2
朽木不可雕也 <aszswaz@163.com>
2022-03-23 23:17:45 +0800

    backup

--- technology/math/pdf.sh
+++ technology/math/pdf.sh
@@ -16,7 +16,9 @@ TEX_FILE=$1
 }
 
 # 删除编译过程中生成的其他文件
-find ! -ipath "*/venv/*" -name "*.out" -or -name "*.log" -or -name "*.aux" | xargs rm -rf
+find ! -ipath "*/venv/*" \
+    -name "*.out" -or -name "*.log" -or -name "*.aux" \
+    -or -name "*.nav" -or -name "*.snm" -or -name "*.toc" -or -name "*.vrb" | xargs rm -rf
 
 # 打开文件
 command -v okular && okular "${TEX_FILE%.*}.pdf"
