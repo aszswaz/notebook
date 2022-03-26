5c914d0b75fa76cdfc80859a0891268a89f575b3
朽木不可雕也 <aszswaz@163.com>
2022-03-27 00:56:46 +0800

    添加导数的描述

--- technology/math/pdf.sh
+++ technology/math/pdf.sh
@@ -4,21 +4,19 @@
 # author: aszswaz
 # date: 2022-03-19
 
+cd $(dirname $0)
 ARGS=($@)
 TEX_FILE=$1
 
 {
     xelatex -output-directory "$(dirname $TEX_FILE)" $TEX_FILE &&
-        echo -e "\033[32mPDF file grenerated successfully.\033[0m"
+        echo -e "\033[32mPDF file grenerated successfully.\033[0m" &&
+        command -v okular >>/dev/null && okular "${TEX_FILE%.*}.pdf"
 } || {
     echo -e "\033[31mPDF file generation failed.\033[0m" >&2
-    exit 1
 }
 
 # 删除编译过程中生成的其他文件
 find ! -ipath "*/venv/*" \
     -name "*.out" -or -name "*.log" -or -name "*.aux" \
     -or -name "*.nav" -or -name "*.snm" -or -name "*.toc" -or -name "*.vrb" | xargs rm -rf
-
-# 打开文件
-command -v okular && okular "${TEX_FILE%.*}.pdf"
